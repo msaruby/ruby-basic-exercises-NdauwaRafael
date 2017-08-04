c9f44ac05324e31f08a1b6d5a06d03d8a9927b9e
NdauwaRafael <raphaelndauwa@gmail.com>
2017-08-04 10:52:34 +0300

    Calculations for assignment question1

--- assignment1.rb
+++ assignment1.rb
@@ -1,13 +1,13 @@
 puts "Enter a Four Digit Number: "
 number = gets.to_i
 
-thousands = number % 1000
-hundreds = number % 100
-tens = number % 10
-ones = number % 1
+thousands = number / 1000
+hundreds = (number%1000)/100
+tens = (number % 100)/10
+ones = (number % 10)/1
 
-5.times do
 
-end
-
-puts "Value in tens is #{thousands}"
+puts "Value in thousands is #{thousands}"
+puts "Value in hundreds is #{hundreds}"
+puts "Value in tens is #{tens}"
+puts "Value in ones is #{ones}"
