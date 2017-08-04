b98a91e12fafd87ad9f070f0a413fafcc9d70895
NdauwaRafael <raphaelndauwa@gmail.com>
2017-08-04 10:27:46 +0300

    Created assignment 1

---
+++ assignment1.rb
@@ -0,0 +1,13 @@
+puts "Enter a Four Digit Number: "
+number = gets.to_i
+
+thousands = number % 1000
+hundreds = number % 100
+tens = number % 10
+ones = number % 1
+
+5.times do
+
+end
+
+puts "Value in tens is #{thousands}"
